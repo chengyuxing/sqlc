3651101ae6395cfaab5607e5f5d7b9a9238b60e2
chengyuxing <chengyuxingo@gmail.com>
2022-12-05 18:08:46 +0800

    命令模式增加指令--with-tx启用事务包裹-e指令

--- src/main/java/com/github/chengyuxing/sql/terminal/cli/Arguments.java
+++ src/main/java/com/github/chengyuxing/sql/terminal/cli/Arguments.java
@@ -14,27 +14,53 @@ public class Arguments {
             "-f",   //format
             "-e",   //sql string or sql file
             "-d",    //multi sql block delimiter};
-            "-header"   // tsv,csv，excel header index
+            "-header",   // tsv,csv，excel header index
+            "--with-tx"   //using transaction wrapper
     };
     private final String[] args;
+    private Map<String, String> argMap = new HashMap<>();
 
     public Arguments(String... args) {
         this.args = args;
+        init();
     }
 
-    public Map<String, String> toMap() {
-        return Stream.of(args).filter(arg -> StringUtil.startsWiths(arg, argNames))
+    void init() {
+        argMap = Stream.of(args).filter(arg -> StringUtil.startsWiths(arg, argNames))
                 .reduce(new HashMap<>(), (acc, curr) -> {
                     for (String name : argNames) {
                         if (curr.startsWith(name)) {
                             int prefixLength = name.length();
                             String key = curr.substring(0, prefixLength);
-                            String value = curr.substring(key.length());
+                            String value = curr.substring(key.length()).trim();
                             acc.put(key, value);
                             break;
                         }
                     }
                     return acc;
                 }, (a, b) -> a);
+    }
+
+    public boolean containsKey(String key) {
+        return argMap.containsKey(key);
+    }
+
+    public String get(String key) {
+        return argMap.get(key);
+    }
+
+    public String getIfBlank(String key, String defaultValue) {
+        if (!argMap.containsKey(key)) {
+            return defaultValue;
+        }
+        if (argMap.get(key).equals("")) {
+            return defaultValue;
+        }
+        return argMap.get(key);
+    }
+
+    @Override
+    public String toString() {
+        return argMap.toString();
     }
 }
