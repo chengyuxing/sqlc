ff7b5b75c4e45cade0e46cfebc8dff94ae1ceb69
chengyuxing <chengyuxingo@gmail.com>
2022-12-08 17:07:45 +0800

    maven打包构建

--- src/main/java/com/github/chengyuxing/sql/terminal/cli/completer/DirectoriesInSqlCompleter.java
+++ src/main/java/com/github/chengyuxing/sql/terminal/cli/completer/DirectoriesInSqlCompleter.java
@@ -1,18 +1,15 @@
 package com.github.chengyuxing.sql.terminal.cli.completer;
 
-import com.github.chengyuxing.common.utils.StringUtil;
 import com.github.chengyuxing.sql.terminal.core.FileHelper;
-import com.github.chengyuxing.sql.terminal.util.SqlUtil;
 import org.jline.builtins.Completers;
 import org.jline.reader.Candidate;
 import org.jline.reader.LineReader;
 import org.jline.reader.ParsedLine;
 
-import java.io.File;
 import java.nio.file.Path;
 import java.util.List;
 
-public class DirectoriesInSqlCompleter extends Completers.DirectoriesCompleter {
+public class DirectoriesInSqlCompleter extends Completers.FilesCompleter {
 
     public DirectoriesInSqlCompleter(Path currentDir) {
         super(currentDir);
